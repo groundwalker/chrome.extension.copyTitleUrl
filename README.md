0b8699446f7bb15c89a10a1d06e1d6a18edf2ce5
Mark Oomori <github@gwu.jp>
2017-02-24 17:31:16 +0900

    説明の修正

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ chrome://extensions へ行く
 `Load unpacked extension` からcloneしたディレクトリを選択してインストール
 
 右下の `Keyboard shortcuts` から `CopyTitleAndUrl` にショートカットを割り当てる
+
+## カスタマイズ
+
+`background.js` の text_area.value を設定しているところを好きに変更するとよいです。
--- background.js
+++ background.js
@@ -6,13 +6,13 @@ chrome.commands.onCommand.addListener(function(command) {
         var tab = tabs[0];
 
         var text_area = document.createElement('textarea');
+        // ここを変更すれば任意の形式にできるよ
         text_area.value = "[" + tab.title + "](" + tab.url + ")";
         document.body.appendChild(text_area);
 
         text_area.select();
         document.execCommand('copy');
 
-        // 3. テキストエリアを削除
         document.body.removeChild(text_area);
     });
 });
